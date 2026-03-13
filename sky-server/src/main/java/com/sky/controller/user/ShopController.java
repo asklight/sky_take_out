245d0f9290461689edbfa81a2a9256a81f604868
asklight <13974300+asklight@user.noreply.gitee.com>
2026-03-14 01:32:03 +0800

    feat: 添加店铺营业状态管理接口，支持设置和获取店铺状态，新增Redis配置

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,33 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "店铺相关接口")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @ApiOperation(value = "获取店铺营业状态")
+    @GetMapping("/status")
+    public Result<Integer> ShopController() {
+
+        Integer status = (Integer) redisTemplate.opsForValue().get("KEY");
+
+        return Result.success(status);
+    }
+
+}
